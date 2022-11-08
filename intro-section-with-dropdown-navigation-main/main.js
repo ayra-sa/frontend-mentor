67421b3481d5600ff58372b89da529057b1dbe2a
arya-sa <rizkyaryas11@gmail.com>
2022-11-08 21:19:09 +0700

    add intro section with dropdown

---
+++ intro-section-with-dropdown-navigation-main/main.js
@@ -0,0 +1,19 @@
+const dropdown = document.querySelectorAll(".dd")
+const dropdownBox = document.querySelectorAll(".dropdown")
+
+for (let i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener('click', () => {
+        console.log(dropdown[i])
+        dropdown[i].classList.toggle('expand')
+    })
+    // for (let j = 0; j < dropdownBox.length; j++) {
+    // }
+}
+
+const navMobile = document.getElementById("nav-mobile")
+const navMenu = document.querySelector(".nav-right")
+
+navMobile.addEventListener("click", () => {
+    navMobile.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
